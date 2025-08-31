4c665b58e8a595944d1f7f098baf5cee87141245
fatherOfChildren568 <contact.me.dothehung@gmail.com>
2025-09-01 00:24:29 +0700

    config name of error in my exception

--- api/ConnectAPI.java
+++ api/ConnectAPI.java
@@ -18,7 +18,8 @@ public class ConnectAPI {
 			// in this case, i use my exception to handle error
 			// advantage of this way, it return clear and exactly error for client
 			ErrorResponseDTO errorResponseDTO = new ErrorResponseDTO();
-			errorResponseDTO.setError("error arithmetic");
+			// use variable e of exception to return :v
+			errorResponseDTO.setError(e.getMessage());
 			List<String> listErrors = new ArrayList<String>();
 			listErrors.add("error division by 0");
 			listErrors.add("test error");
