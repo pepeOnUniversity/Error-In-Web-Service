5724dc6fb6a538d8e523a4fa639fb61451b3eff1
fatherOfChildren568 <contact.me.dothehung@gmail.com>
2025-09-01 23:09:03 +0700

    class store object return from client

---
+++ beans/BuildingDTO.java
@@ -0,0 +1,35 @@
+package com.error.beans;
+
+public class BuildingDTO {
+	private String name;
+	private Integer floor;
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the floor
+	 */
+	public Integer getFloor() {
+		return floor;
+	}
+
+	/**
+	 * @param floor the floor to set
+	 */
+	public void setFloor(Integer floor) {
+		this.floor = floor;
+	}
+
+}
