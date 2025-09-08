a22d1550567bdb0f82dab03555476502f5d1c25e
fatherOfChildren568 <contact.me.dothehung@gmail.com>
2025-09-08 22:39:35 +0700

    test ControllerAdvice

--- api/ConnectAPI.java
+++ api/ConnectAPI.java
@@ -1,41 +1,19 @@
 package com.error.api;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.error.beans.BuildingDTO;
-import com.error.beans.ErrorResponseDTO;
-import com.error.customException.MyException;
 
 @RestController
 public class ConnectAPI {
 
 	@PostMapping(value = "/connect/")
 	public Object pullBuilding(@RequestBody BuildingDTO buildingDTO) {
-		try {
-			validate(buildingDTO);
-		} catch (Exception e) {
-			// TODO: handle exception
-
-			ErrorResponseDTO errorResponseDTO = new ErrorResponseDTO();
-			errorResponseDTO.setError(e.getMessage());
-			List<String> listErrors = new ArrayList<String>();
-			listErrors.add("Detail error 1");
-			listErrors.add("Detail error 2");
-			errorResponseDTO.setDetailError(listErrors);
-			return errorResponseDTO;
-		}
+		// this error ==> ping controlle advice
+		System.out.println(5 / 0);
 		return null;
 	}
 
-	// this function return error
-	public void validate(BuildingDTO buildingDTO) {
-		if (buildingDTO.getName() == null || buildingDTO.getName().equals("") || buildingDTO.getFloor() == null) {
-			throw new MyException("message error in functoin validate");
-		}
-	}
 }
