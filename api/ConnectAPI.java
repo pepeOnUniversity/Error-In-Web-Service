5ed801c2486c235e3512800113bbe10753852347
pepeOnUniversity <contact.me.dothehung@gmail.com>
2025-10-07 16:23:05 +0700

    handle null from request

--- api/ConnectAPI.java
+++ api/ConnectAPI.java
@@ -5,15 +5,22 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.error.beans.BuildingDTO;
+import com.error.customException.MyException;
 
 @RestController
 public class ConnectAPI {
 
 	@PostMapping(value = "/connect/")
 	public Object pullBuilding(@RequestBody BuildingDTO buildingDTO) {
-		// this error ==> ping controlle advice
-		System.out.println(5 / 0);
+		String nameString = buildingDTO.getName();
+		int floor = buildingDTO.getFloor();
+//		if (nameString == null) {
+//			throw new MyException("Name không được null");
+//		}
+
+		if (floor > 10) {
+			throw new MyException("The floor excess 10");
+		}
 		return null;
 	}
-
 }
